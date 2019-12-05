150f9c85124add8cabb2841d67913b4f916ad8c8
alexpt253 <56148814+alexpt253@users.noreply.github.com>
2019-12-05 15:14:49 +1100

    Update fn_load_tb_policy_period.sql

--- fn_load_tb_policy_period.sql
+++ fn_load_tb_policy_period.sql
@@ -8,6 +8,18 @@ BEGIN
 
 truncate table tb_policy_period;
 
+with cte1 as (
+  select sar_policy as policy_term_number,
+  cast (sar_trans_eff_date as date) as sar_trans_eff_date, 
+  cast (sar_expiry_date as date) as sar_expiry_date,
+  cast (sar_cov_eff_date as date) as sar_cov_eff_date,
+  sar_transaction
+  from sor_pms_vsam.sor_rec4514 
+  where substr(sar_policy,1,10)='23A0231594'
+  group by policy_term_number, sar_trans_eff_date, sar_expiry_date, sar_transaction, sar_cov_eff_date
+  order by policy_term_number, sar_trans_eff_date, sar_expiry_date,sar_transaction, sar_cov_eff_date
+)
+
 INSERT INTO tb_policy_period ( 
 policy_term_number,
 coverable_id,
@@ -16,9 +28,20 @@ policy_period_start,
 policy_period_end,
 flag_active_row 
 )
-SELECT
 
-
+SELECT policy_term_number,
+case when sar_transaction in ('10','11','12') then sar_trans_eff_date 
+     else sar_cov_eff_date
+     end as policy_period_start,
+case when sar_transaction in ('10','11','12') then sar_expiry_date
+     else sar_trans_eff_date
+     end as policy_period_end,
+case when sar_transaction in ('10','11','12') then 'Y'
+     else 'N'
+     end as flag_active_row
+from cte1
+where sar_transaction not in ('53','63')
+order by policy_term_number,policy_period_start;
 
 out_function_status := 'SUCCESS';
 
