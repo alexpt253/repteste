bd079377a1a1af91d94e9ac44ec7093d219d6ee4
alexpt253 <56148814+alexpt253@users.noreply.github.com>
2019-12-05 14:32:53 +1100

    Update fn_load_tb_policy_period.sql

--- fn_load_tb_policy_period.sql
+++ fn_load_tb_policy_period.sql
@@ -1,1 +1,35 @@
+CREATE OR REPLACE FUNCTION fn_load_tb_policy_period(OUT out_function_status text, OUT out_function_error_message text)
 
+
+RETURNS RECORD AS 
+$BODY$
+
+BEGIN
+
+truncate table tb_policy_period;
+
+INSERT INTO tb_policy_period ( 
+policy_term_number,
+coverable_id,
+coverage_id,
+policy_period_start,
+policy_period_end,
+flag_active_row 
+)
+SELECT
+
+
+
+out_function_status := 'SUCCESS';
+
+RETURN;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    out_function_status := 'FAIL';
+      out_function_error_message := SQLSTATE||': '||SQLERRM;
+      RETURN;
+
+END $BODY$
+
+LANGUAGE plpgsql VOLATILE SECURITY INVOKER;
